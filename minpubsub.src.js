d063273437fc18726624be2d34f8a1de34e37e68
Daniel Lamb <daniellmb@hotmail.com>
2011-05-04 16:01:34 -0700

    updated license from the joke WTFPL to the standard MIT

--- minpubsub.src.js
+++ minpubsub.src.js
@@ -1,8 +1,8 @@
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details. */ 
+/*!
+* MinPubSub
+* Copyright(c) 2011 Daniel Lamb <daniellmb.com>
+* MIT Licensed
+*/
 
 (function(d){
 
